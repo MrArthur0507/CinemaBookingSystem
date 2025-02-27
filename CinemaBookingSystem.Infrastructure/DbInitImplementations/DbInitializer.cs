9fd39e19acd04705afe31b3da80f920b2a0711a0
MrArthur0507 <mrarthur0507@gmail.com>
2025-02-27 17:10:52 +0200

    defined contracts for db init

---
+++ CinemaBookingSystem.Infrastructure/DbInitImplementations/DbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using CinemaBookingSystem.Application.DbInitializationContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaBookingSystem.Infrastructure.DbInitImplementations
+{
+    public class DbInitializer : IDbInitializer
+    {
+        public bool CreateDbIfDoesNotExist()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CinemaBookingSystem.Infrastructure/DbInitImplementations/MSServerCreationQuery.cs
@@ -0,0 +1,17 @@
+﻿using CinemaBookingSystem.Application.DbInitializationContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaBookingSystem.Infrastructure.DbInitImplementations
+{
+    public class MSServerCreationQuery : IDbCreationQuery
+    {
+        public bool ExecuteCreationQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
