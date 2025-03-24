215f79119451b16899cd4eecc16fe956d74d455e
MrArthur0507 <mrarthur0507@gmail.com>
2025-03-24 15:26:28 +0200

    movie dtos

---
+++ CinemaBookingSystem.Application/DTOs/MovieDtos/CreateMovieDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaBookingSystem.Application.DTOs.MovieDtos
+{
+    public class CreateMovieDto
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ CinemaBookingSystem.Application/DTOs/MovieDtos/UpdateMovieDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaBookingSystem.Application.DTOs.MovieDtos
+{
+    public class UpdateMovieDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
