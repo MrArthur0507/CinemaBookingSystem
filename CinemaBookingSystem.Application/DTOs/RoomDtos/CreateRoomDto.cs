fb7619ef315ad66ebab43b5bc9919c86ec7cb112
MrArthur0507 <mrarthur0507@gmail.com>
2025-04-07 14:08:00 +0300

    Created room dtos

---
+++ CinemaBookingSystem.Application/DTOs/RoomDtos/CreateRoomDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaBookingSystem.Application.DTOs.RoomDtos
+{
+    public class CreateRoomDto
+    {
+        public string RoomName { get; set; }
+        public Guid CinemaLocationId { get; set; }
+    }
+}
