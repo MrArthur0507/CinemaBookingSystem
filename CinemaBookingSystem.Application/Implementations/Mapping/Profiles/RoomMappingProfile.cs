d1141309ab8f8fc50c9b92a40b07a2e7bb021d12
MrArthur0507 <mrarthur0507@gmail.com>
2025-04-07 14:09:32 +0300

    Created room mapping profile

---
+++ CinemaBookingSystem.Application/Implementations/Mapping/Profiles/RoomMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CinemaBookingSystem.Application.DTOs.RoomDtos;
+using CinemaBookingSystem.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaBookingSystem.Application.Implementations.Mapping.Profiles
+{
+    public class RoomMappingProfile : Profile
+    {
+        public RoomMappingProfile()
+        {
+            CreateMap<Room, RoomDto>().ReverseMap();
+            CreateMap<CreateRoomDto, Room>();
+            CreateMap<UpdateRoomDto, Room>();
+        }
+    }
+}
